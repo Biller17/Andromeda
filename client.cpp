d6985b8e97435513d2484eedb8164c2967adf193
Biller17 <abiller95@gmail.com>
2017-04-21 11:51:48 -0500

    --

--- client.cpp
+++ client.cpp
@@ -1,19 +1,4 @@
 
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
-
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include "ResourcePath.hpp"
@@ -31,28 +16,33 @@
 
 void usage(char * program);
 void connectToServer(char * address, char * port);
-void cardGame(int connection_fd);
-
+void communicationLoop(int connection_fd);
 
 int main(int argc, char * argv[])
 {
-    
-    
-    if (argc != 3)
-        usage(argv[0]);
-    
-    connectToServer(argv[1], argv[2]);
+    float spaceshipPosY = 550;
+    float spaceshipPosX = 500;
+   // connectToServer("255.255.255.255", "80");
     
     
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::RenderWindow window(sf::VideoMode(1000, 600), "SFML window");
 
     // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
+    /*sf::Image icon;
+    if (!icon.loadFromFile(resourcePath() + "spaceship.png")) {
         return EXIT_FAILURE;
     }
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+     */
+    
+    sf::Texture spaceship;
+    if (!spaceship.loadFromFile(resourcePath() + "spaceship.png")) {
+        return EXIT_FAILURE;
+    }
+    sf::Sprite spaceshipSprite(spaceship);
+    
 
     // Load a sprite to display
     sf::Texture texture;
@@ -93,7 +83,12 @@ int main(int argc, char * argv[])
 
         // Draw the sprite
         window.draw(sprite);
-
+        
+        window.draw(spaceshipSprite);
+        
+        spaceshipSprite.setPosition(spaceshipPosX, spaceshipPosY);
+    
+        
         // Draw the string
         window.draw(text);
 
@@ -160,23 +155,20 @@ void connectToServer(char * address, char * port)
     freeaddrinfo(server_info);
     
     // Establish the communication
-    cardGame(connection_fd);
+    communicationLoop(connection_fd);
     
     // Close the socket
     close(connection_fd);
 }
 
 
-void cardGame(int connection_fd)
+void communicationLoop(int connection_fd)
 {
     char buffer[BUFFER_SIZE];
-    int hand[BUFFER_SIZE];
-    int extraCard = 0;
-    int totalPoints;
     
     while (1)
     {
-        printf("Welcome to Black Jack Online \n do you wish to play? (yes = y/no = enter)");
+        printf("Enter a message for the server (empty message to finish): ");
         fgets(buffer, BUFFER_SIZE, stdin);
         
         // Finish the connection with a string containing only the '\n'
@@ -199,61 +191,12 @@ void cardGame(int connection_fd)
         
         ///// RECV
         // Read the request from the client
-        if(read(connection_fd,hand, BUFFER_SIZE) == -1 ){
-            perror("ERROR: recv");
-            exit(EXIT_FAILURE);
-        }
-        printf("These are your cards\nCard 1: %d \nCard 2: %d\n", hand[0], hand[1]);
-        
-        
-        //asking to the dealer for another card
-        printf("Do you wish to get another card? (yes = y / no = enter)\n");
-        fgets(buffer, BUFFER_SIZE, stdin);
-        if ( send(connection_fd, buffer, strlen(buffer)+1, 0) == -1 ){
-            perror("ERROR: send");
-            exit(EXIT_FAILURE);
-        }
-        
-        
-        //receiving extra card
-        if(strlen(buffer) > 1){
-            if(read(connection_fd, &extraCard, BUFFER_SIZE) == -1){
-                perror("ERROR: read");
-                exit(EXIT_FAILURE);
-            }
-            printf("Dealer has dealt you a %d\n", extraCard);
-        }
-        
-        //receiving total points
-        if(read(connection_fd, &totalPoints, 4) == -1 ){
-            perror("ERROR: read");
-            exit(EXIT_FAILURE);
-        }
-        printf("Your total points are %d\n", totalPoints);
-        
-        
-        //Receiving who's the winner
-        if(read(connection_fd, &buffer, BUFFER_SIZE) == -1)
+        if ( recv(connection_fd, buffer, BUFFER_SIZE, 0) == -1 )
         {
             perror("ERROR: recv");
             exit(EXIT_FAILURE);
         }
-        
-        printf("%s\n",buffer);
-        printf("Do you wish to play again? (y = yes/ enter = no)");
-        
-        
-        fgets(buffer, BUFFER_SIZE, stdin);
-        
-        // Finish the connection with a string containing only the '\n'
-        if (strlen(buffer) == 1)
-        {
-            printf("Finishing the connection\n");
-            break;
-        }
-        
-        
-        
+        printf("The server replied with: %s\n", buffer);
     }
 }
 
